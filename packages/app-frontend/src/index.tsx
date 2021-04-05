adc55ce04c57d1375e97a13d77ae331df8e6df73
marviel <lukebechtel4@gmail.com>
2021-04-05 10:00:15 -0400

    Update Index with UserConcept Generation

--- packages/app-frontend/src/index.tsx
+++ packages/app-frontend/src/index.tsx
@@ -8,6 +8,11 @@ import {Provider} from 'react-redux';
 import {ThemeProvider} from '@material-ui/core'
 import { materialUItheme } from './style/materialUITheme';
 import { ConnectedRouter } from 'connected-react-router'
+import _ from 'lodash';
+import { uuid } from './utils/uuidUtils';
+import { notEmpty } from './utils/typeUtils';
+import { userConceptAdded } from './state/slices/userConceptSlice';
+import { UserConcept } from './models/UserConcept';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -26,3 +31,18 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+_.values(store.getState().concepts.entities).filter(notEmpty).forEach(async (concept) => {
+  const userConcept: UserConcept = {
+    id: uuid(),
+    userId: "DUMMY_USER",
+    conceptId: concept.id,
+    successfulConsecutiveReviews: 0,
+    srsData: {
+        srsDataType: "SuperMemo2SRSData",
+        easinessFactor: 2.5,
+        nextReviewTime: new Date()
+    }
+  }
+  store.dispatch(userConceptAdded(userConcept))
+})
