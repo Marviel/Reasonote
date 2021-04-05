01acb890acf7ff66331d29d5bbcd6686e5ae8497
marviel <lukebechtel4@gmail.com>
2021-04-05 09:09:59 -0400

    Remove extraneous variable from Concept model

--- packages/app-frontend/src/models/Concept.ts
+++ packages/app-frontend/src/models/Concept.ts
@@ -4,7 +4,6 @@ import { ReasonoteExercise } from './Exercise/ReasonoteExercise';
 import { UserExerciseInstance } from './UserExerciseInstance';
 
 export const Concept = z.object({
-    exerciseType: z.string(),
     id: z.string().uuid(),
     name: z.string(),
     preRequisites: z.array(z.string()),
