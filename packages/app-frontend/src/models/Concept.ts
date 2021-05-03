4b749483ce21437b73154ef68ff77563a4b5c928
marviel <lukebechtel4@gmail.com>
2021-05-03 00:58:58 -0400

    fix Concept model

--- packages/app-frontend/src/models/Concept.ts
+++ packages/app-frontend/src/models/Concept.ts
@@ -4,7 +4,7 @@ import { ReasonoteExercise } from './Exercise/ReasonoteExercise';
 import { UserExerciseInstance } from './UserExerciseInstance';
 
 export const Concept = z.object({
-    id: z.string().uuid(),
+    id: z.string(),
     name: z.string(),
     preRequisites: z.array(z.string()),
 });
